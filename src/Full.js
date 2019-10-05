7be7cc5a604a18e3c360078fdfc8af871d84fe9d
Ikemefuna Obioha <ikemoobioha@gmail.com>
2019-10-05 04:29:59 +0100

    Initial project setup

---
+++ src/Full.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Calc from "./components/Calc";
+
+class Full extends Component {
+	render() {
+    return (
+      <div className="container">
+        <Calc />
+      </div>  
+    );
+  }
+}
+ 
+export default Full
---
+++ src/components/Calc.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Calc extends Component {
+  render() {
+    return (
+      <div style={{ flexDirection: "row", display: "flex" }}></div>
+    );
+  }
+}
+
+export default Calc
